eb14b5c860cbaa8010021c6baa9bbc4569bf40cc
Md. Al-Amin <alaminopu.me@gmail.com>
2021-02-22 04:11:14 +0100

    Fixed project

--- marvel_api/config.py
+++ marvel_api/config.py
@@ -1,9 +1,12 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#Give access to the project in ANY OS we find ourlselves in
-#Allow outside files / folders to be added to the project
-#from the base directory
+# Give access to the project in ANY OS we find ourlselves in
+# Allow outside files / folders to be added to the project
+# from the base directory
+
+LOCAL_DB_URL = 'postgresql://postgres:Robert1972@127.0.0.1:5432/marvel_api'
+
 
 class Config():
     """
@@ -13,6 +16,5 @@ class Config():
 
     """
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Robert1972@127.0.0.1:5432/marvel_api'
-    print(SQLALCHEMY_DATABASE_URI)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False #Turn off update messages
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or LOCAL_DB_URL
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # Turn off update messages
